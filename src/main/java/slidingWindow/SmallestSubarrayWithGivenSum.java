1d22c4d6d130ed7dc38b2355cda96ba1bee497c3
Nimish Singh <nimishs@thoughtworks.com>
2021-03-03 09:52:36 +0530

    Simplifies logic for sliding window over 'minimum length of subarray with sum >= s'

--- src/main/java/slidingWindow/SmallestSubarrayWithGivenSum.java
+++ src/main/java/slidingWindow/SmallestSubarrayWithGivenSum.java
@@ -6,16 +6,12 @@ public class SmallestSubarrayWithGivenSum {
         int minWindowSize = Integer.MAX_VALUE, windowStart = 0, sum = 0;
 
         for (int windowEnd = 0; windowEnd < arr.length; windowEnd++) {
-            while (windowEnd < arr.length && sum + arr[windowEnd] < s) {
-                sum += arr[windowEnd++];
-            }
-            if (windowEnd < arr.length)
-                sum += arr[windowEnd];
+            sum += arr[windowEnd];
 
-            while (windowStart <= windowEnd && sum - arr[windowStart] >= s) {
+            while (sum >= s) {
+                minWindowSize = Math.min(minWindowSize, windowEnd - windowStart + 1);
                 sum -= arr[windowStart++];
             }
-            minWindowSize = Math.min(minWindowSize, windowEnd - windowStart + 1);
         }
 
         return minWindowSize;
