5c1839115137596f696f4f62a07f817151b67500
Nimish Singh <nimishs@Nimishs-MacBook-Pro.local>
2022-07-26 22:44:01 +0530

    Adds problem to find sum of elements of matrix between given indexes

---
+++ src/main/java/rangeSum/NumMatrix.java
@@ -0,0 +1,33 @@
+package rangeSum;
+
+public class NumMatrix {
+    private int[][] prefixSums;
+
+    public NumMatrix(int[][] matrix) {
+        prefixSums = new int[matrix.length][matrix[0].length];
+
+        for (int row = 0; row < prefixSums.length; row++) {
+            for (int column = 0; column < prefixSums[0].length; column++) {
+                if (column == 0) {
+                    prefixSums[row][column] = matrix[row][column];
+                    continue;
+                }
+
+                prefixSums[row][column] = prefixSums[row][column - 1] + matrix[row][column];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int row = row1; row <= row2; row++) {
+            if (col1 == 0) {
+                sum += prefixSums[row][col2];
+                continue;
+            }
+
+            sum += prefixSums[row][col2] - prefixSums[row][col1 - 1];
+        }
+        return sum;
+    }
+}
---
+++ src/main/test/rangeSum/NumMatrixTest.java
@@ -0,0 +1,23 @@
+package rangeSum;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumMatrixTest {
+    private NumMatrix nm;
+
+    @Test
+    public void sampleInput1() {
+        int[][] matrix = new int[][]{{3, 0, 1, 4, 2},
+                                  {5, 6, 3, 2, 1},
+                                  {1, 2, 0, 1, 5},
+                                  {4, 1, 0, 1, 7},
+                                  {1, 0, 3, 0, 5}};
+        nm = new NumMatrix(matrix);
+
+        assertEquals(8, nm.sumRegion(2, 1, 4, 3));
+        assertEquals(11, nm.sumRegion(1, 1, 2, 2));
+        assertEquals(12, nm.sumRegion(1, 2, 2, 4));
+    }
+}
