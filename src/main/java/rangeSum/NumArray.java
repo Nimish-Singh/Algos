e3826bb185eae5a2b402bd268de163ad5ef1270f
Nimish Singh <nimishs@Nimishs-MacBook-Pro.local>
2022-07-26 21:14:43 +0530

    Adds problem to find sum of elements of array between given indexes

---
+++ src/main/java/rangeSum/NumArray.java
@@ -0,0 +1,23 @@
+package rangeSum;
+
+// https://leetcode.com/problems/range-sum-query-immutable/
+public class NumArray {
+    private int[] prefixSumArray;
+
+    public NumArray(int[] nums) {
+        prefixSumArray = new int[nums.length];
+
+        prefixSumArray[0] = nums[0];
+        for (int index = 1; index < prefixSumArray.length; index++) {
+            prefixSumArray[index] = prefixSumArray[index - 1] + nums[index];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        if (left == 0) {
+            return prefixSumArray[right];
+        }
+
+        return prefixSumArray[right] - prefixSumArray[left - 1];
+    }
+}
---
+++ src/main/test/rangeSum/NumArrayTest.java
@@ -0,0 +1,19 @@
+package rangeSum;
+
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class NumArrayTest {
+    private NumArray numArray;
+
+    @Test
+    public void sampleInput1() {
+        int[] arr = new int[]{-2, 0, 3, -5, 2, -1};
+        numArray = new NumArray(arr);
+
+        assertEquals(1, numArray.sumRange(0, 2));
+        assertEquals(-1, numArray.sumRange(2, 5));
+        assertEquals(-3, numArray.sumRange(0, 5));
+    }
+}
